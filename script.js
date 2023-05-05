8fbe4517ebae21b0b80accd0a1a2d3c6a4e51fc3
rolanusonov <rolanusonov031209@gmail.com>
2023-05-05 12:19:03 +0900

    inet

--- script.js
+++ script.js
@@ -26,9 +26,9 @@ let map;
 
 DG.then(function () {
     map = DG.map('map', {
-        center: [54.98, 82.89],
+        center: [65.952040, 111.476797],
         zoom: 13
     });
 
-    DG.marker([54.98, 82.89]).addTo(map).bindPopup('Вы кликнули по мне!');
+    DG.marker([65.952040, 111.476797]).addTo(map).bindPopup('Шашлычный');
 });
